57f0905328d965119642964a609d0093f62b320e
leo lam <lamfung0@hotmail.com.hk>
2023-05-25 18:29:25 +0800

    first try

---
+++ guess_num.py
@@ -0,0 +1,21 @@
+# 產生一個隨機正式1~100 (不要印出來)
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出 "終於猜對了!"
+# 猜錯的話 印出 "比答案大/小"
+
+import random
+
+ans = random.randint(1, 100)
+ans = int(ans)
+
+while True:
+    guess = input("請猜1~100之間的任何一個數字: ")
+    guess = int(guess)
+    if guess == ans:
+        print("終於猜對了!")
+        break
+    elif guess > ans:
+        print("你的猜測比正確答案大")
+    elif guess < ans:
+        print("你的猜測比正確答案小")
+        
